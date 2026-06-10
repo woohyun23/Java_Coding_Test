4baa64d9d94bd846c9234392ad4af615787bae7a
woohyun23 <parkwh0823@gmail.com>
2026-06-10 22:36:10 +0900

    solve: 인프런 2-11 임시반장 정하기

---
+++ solutions/inflearn/section02/problem11/Main.java
@@ -0,0 +1,39 @@
+package inflearn.section02.problem11;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][5];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(solution(n, arr));
+
+    }
+    public static int solution(int n, int[][] arr) {
+        int res = 1;
+        int maxCount = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > maxCount) {
+                maxCount = cnt;
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+}
