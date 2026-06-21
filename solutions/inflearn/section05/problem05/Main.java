5ae6f3f6243728c637b6dce8fdfcfc5b824b1b74
woohyun23 <parkwh0823@gmail.com>
2026-06-22 07:59:57 +0900

    solve: 인프런 5-5 쇠막대기

---
+++ solutions/inflearn/section05/problem05/Main.java
@@ -0,0 +1,16 @@
+package inflearn.section05.problem05;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(solution(input));
+    }
+    public static int solution(String input) {
+
+
+        return 0;
+    }
+}
