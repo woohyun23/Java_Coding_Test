221f5800a4ea42e8f7c5ef20ed83feaf7fdad2ce
woohyun23 <parkwh0823@gmail.com>
2026-06-24 12:53:51 +0900

    solve: 프로그래머스 12947 하샤드 수

---
+++ solutions/programmers/problem12947/Main.java
@@ -0,0 +1,8 @@
+package programmers.problem12947;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ solutions/programmers/problem12947/Solution.java
@@ -0,0 +1,16 @@
+package programmers.problem12947;
+
+class Solution {
+    public boolean solution(int input) {
+        boolean res = true;
+        int temp = 0;
+        String s = String.valueOf(input);
+        for (char x : s.toCharArray()) {
+            temp += Integer.parseInt(String.valueOf(x));
+        }
+
+        if (input % temp != 0) res = false;
+        return res;
+    }
+
+}
