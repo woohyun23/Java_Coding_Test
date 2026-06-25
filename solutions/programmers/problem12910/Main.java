4bd92e3755c63fe11b6c7ecbfc95660a10d63cc6
woohyun23 <parkwh0823@gmail.com>
2026-06-25 11:04:20 +0900

    solve: 프로그래머스 12910 나누어 떨어지는 숫자 배열

---
+++ solutions/programmers/problem12910/Main.java
@@ -0,0 +1,8 @@
+package programmers.problem12910;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ solutions/programmers/problem12910/Solution.java
@@ -0,0 +1,21 @@
+package programmers.problem12910;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        for (int x : arr) {
+            if (x % divisor == 0) {
+                answer = java.util.Arrays.copyOf(answer, answer.length + 1);
+                answer[answer.length - 1] = x;
+            }
+        }
+        if (answer.length == 0) {
+            answer = java.util.Arrays.copyOf(answer, 1);
+            answer[0] = -1;
+        }
+        else Arrays.sort(answer);
+        return answer;
+    }
+}
