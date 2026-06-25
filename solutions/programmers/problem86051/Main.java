0c6b9a9d4470e7ac2649d8301c8c71a5dfb0a8ba
woohyun23 <parkwh0823@gmail.com>
2026-06-25 11:07:13 +0900

    solve: 프로그래머스 86051 없는 숫자 더하기

---
+++ solutions/programmers/problem86051/Main.java
@@ -0,0 +1,8 @@
+package programmers.problem86051;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ solutions/programmers/problem86051/Solution.java
@@ -0,0 +1,18 @@
+package programmers.problem86051;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
+
+        for (int x : numbers) {
+            arr[x] = 0;
+        }
+
+        for (int x : arr) answer += x;
+        return answer;
+    }
+}
