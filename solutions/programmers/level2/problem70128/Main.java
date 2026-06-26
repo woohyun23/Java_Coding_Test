c342b31d6c11ba0e74d9dd9a0a837358ab964ced
woohyun23 <parkwh0823@gmail.com>
2026-06-26 10:58:43 +0900

    solve: 프로그래머스 70128 내적

---
+++ solutions/programmers/level2/problem70128/Main.java
@@ -0,0 +1,8 @@
+package programmers.level2.problem70128;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ solutions/programmers/level2/problem70128/Solution.java
@@ -0,0 +1,13 @@
+package programmers.level2.problem70128;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
