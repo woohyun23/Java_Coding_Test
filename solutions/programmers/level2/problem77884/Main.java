53d87a26367a8048cd109a907ec0333efee7e63c
woohyun23 <parkwh0823@gmail.com>
2026-06-29 11:16:36 +0900

    solve: 프로그래머스 77884 약수의 개수와 덧셈

---
+++ solutions/programmers/level2/problem77884/Main.java
@@ -0,0 +1,8 @@
+package programmers.level2.problem77884;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ solutions/programmers/level2/problem77884/Solution.java
@@ -0,0 +1,23 @@
+package programmers.level2.problem77884;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            if (findDivisor(i) % 2 == 0) answer += i;
+            else answer -= i;
+        }
+        return answer;
+    }
+
+    public int findDivisor (int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) cnt++;
+        }
+
+        return  cnt;
+    }
+}
