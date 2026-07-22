978ec340974b8f41966644bc9beb613ec01ac885
woohyun23 <parkwh0823@gmail.com>
2026-07-22 14:06:21 +0900

    solve: 프로그래머스 12915 문자열 내 마음대로 정렬하기

---
+++ solutions/programmers/level3/problem12915/Solution.java
@@ -0,0 +1,15 @@
+package programmers.level3.problem12915;
+
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+
+        Arrays.sort(strings, (s1, s2) -> {
+            if (s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+            return s1.charAt(n) - s2.charAt(n);
+        });
+
+        return strings;
+    }
+}
