55f95ccc18caa5e21495558268e7939487875fd0
woohyun23 <parkwh0823@gmail.com>
2026-07-24 12:33:50 +0900

    solve: 프로그래머스 132267 콜라 문제

---
+++ solutions/programmers/level3/problem132267/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level3.problem132267;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int n) {
+        // a : 마트에 가져다 주어야할 병의 개수
+        // b : 빈 병 a개를 가져다주면 마트에서 주는 콜라 병 수
+        // n : 상빈이가 가지고 있는 빈 병의 수
+        int answer = 0;
+
+        return answer;
+    }
+}
