15944820f155fac410bad22b8f7c88d5083390fd
woohyun23 <parkwh0823@gmail.com>
2026-08-19 11:19:00 +0900

    solve: 프로그래머스 159994 카드 뭉치

---
+++ solutions/programmers/level3/problem159994/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level3.problem159994;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0, j = 0;
+
+        for (int k = 0; k < goal.length; k++) {
+            if (i < cards1.length && goal[k].equals(cards1[i])) i++;
+            else if (j < cards2.length && goal[k].equals(cards2[j])) j++;
+            else return "NO";
+        }
+        return "YES";
+    }
+}
